1ff7ca0a27c125051fdfa187cc7349279e65e215
Mark Schmieder <markhschmieder@gmail.com>
2025-07-14 16:07:54 -0400

    Update MouseDragContext.java
    It is helpful to catch the drag offsets as it allows us to push more of the logic out of copy/paste code between applications and into this core library, which makes sense as there's no need for varied handling of such simple geometric calcs.

--- src/main/java/com/mhschmieder/fxgraphicstoolkit/input/MouseDragContext.java
+++ src/main/java/com/mhschmieder/fxgraphicstoolkit/input/MouseDragContext.java
@@ -32,24 +32,34 @@ package com.mhschmieder.fxgraphicstoolkit.input;
 
 /**
  * This class stores contextual information and settings for Mouse Drag and
- * related handling.
+ * related handling. All values are stored in pixels, for mouse interaction.
  * <p>
  * TODO: Add the ALT key detection etc., and change to more general name?
  *  Possibly add the drag box and its handling as well?
  */
 public class MouseDragContext {
 
-    public double  _dragDestinationX;
-    public double  _dragDestinationY;
     public double  _dragOriginX;
     public double  _dragOriginY;
+
+    public double  _dragDestinationX;
+    public double  _dragDestinationY;
+
+    public double  _dragDeltaX;
+    public double  _dragDeltaY;
+
     public boolean _valid;
 
     public MouseDragContext() {
-        _dragDestinationX = 0.0d;
-        _dragDestinationY = 0.0d;
         _dragOriginX = 0.0d;
         _dragOriginY = 0.0d;
+        
+        _dragDestinationX = 0.0d;
+        _dragDestinationY = 0.0d;
+        
+        _dragDeltaX = 0.0d;
+        _dragDeltaY = 0.0d;
+        
         _valid = false;
     }
 
@@ -57,11 +67,49 @@ public class MouseDragContext {
     // NOTE: The first Drag Event is thrown out, so we zero the destination.
     //  This ensures that the deltas stay scaled properly on each succession.
     public void initializeDrag( final double firstX, final double firstY ) {
-        _dragDestinationX = 0.0d;
-        _dragDestinationY = 0.0d;
         _dragOriginX = firstX;
         _dragOriginY = firstY;
+
+        _dragDestinationX = 0.0d;
+        _dragDestinationY = 0.0d;
+
         _valid = true;
+    }
+    
+    /**
+     * Drags the contextual origin and destination by a computed delta and
+     * caches that delta for the caller to use when converting to meters etc.
+     * 
+     * @param cursorCoordinatesPixels
+     *            The current coordinates of the cursor, in pixels
+     */
+    public void dragTo( final ClickLocation cursorCoordinatesPixels ) {
+        // Update the x and y deltas for the drag and drop operation by 
+        // computing the x and y differentials between the current mouse 
+        // location and the previous mouse location, in pixels.
+        _dragDeltaX = cursorCoordinatesPixels.x - _dragOriginX;
+        _dragDeltaY = cursorCoordinatesPixels.y - _dragOriginY;
+
+        // Try for a coarser resolution, to improve performance.
+        // NOTE: This causes a perception of lag or jumpiness, vs. smooth and
+        //  high resolution dragging, and the performance issue no longer seems
+        //  present even with large selection sets being dragged, as other
+        //  improvements were made since this performance fix was first
+        //  implemented. If we re-enable, perhaps use a smaller threshold.
+        // if ( ( FastMath.abs( _dragDeltaX ) <= 4.0d )
+        //         && ( FastMath.abs( _dragDeltaY ) <= 4.0d ) ) {
+        //     return;
+        // }
+
+        // Replace the Drag Origin with the current cursor coordinates for the
+        // next mouse drag/move, as otherwise we accelerate over time.
+        _dragOriginX = cursorCoordinatesPixels.x;
+        _dragOriginY = cursorCoordinatesPixels.y;
+
+        // Keep track of the cumulative drag deltas calculated from the current
+        // mouse cursor movement, to use for node placement.
+        _dragDestinationX += _dragDeltaX;
+        _dragDestinationY += _dragDeltaY;       
     }
 
     // This is the initializer when interacting with Move events.
@@ -72,10 +120,12 @@ public class MouseDragContext {
                                 final double firstY,
                                 final double lastX,
                                 final double lastY ) {
-        _dragDestinationX = lastX;
-        _dragDestinationY = lastY;
         _dragOriginX = firstX;
         _dragOriginY = firstY;
+
+        _dragDestinationX = lastX;
+        _dragDestinationY = lastY;
+
         _valid = true;
     }
 
